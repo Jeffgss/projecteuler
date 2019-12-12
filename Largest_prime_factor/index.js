0d9565eb19976375b3e47137843d96c8f3ffce48
Jefferson Gerson dos Santos <jefferson.santos@ENG-2589.maxmilhas.local>
2019-12-12 18:08:33 -0300

    Finalizando exercício 3

---
+++ Largest_prime_factor/index.js
@@ -0,0 +1,11 @@
+let valor = 600851475143;
+let x = 0;
+for (let index = 2; index < valor; index++) {
+    console.log(`${index}/${valor}`)
+    if(valor % index  === 0) {
+        valor = valor/ index;
+        x = x < index ? index : x;
+    }
+    
+}
+console.log(x);
