382cb7dc97f6027d192ebf11b13df7f46a8ebffc
Jefferson Gerson dos Santos <jefferson.santos@ENG-2589.maxmilhas.local>
2019-12-12 14:42:59 -0300

    Finalizando o problema 2

---
+++ Even_Fibonacci_numbers/index.js
@@ -0,0 +1,15 @@
+n1 = 3;
+n2 = [0,1,1];
+n3 = 0;
+while(n3 < 10 ){
+    n22 = n1 - 2;
+    n11 = n1 - 1;
+    n2.push(n2[n11] + n2[n22]);
+    n3++;
+    n1++;
+}
+const soma = n2.reduce((acc, valueAtual ) => {
+    if( valueAtual % 2 === 0) return acc + valueAtual;
+    return acc;
+},0)
+console.log(soma)
