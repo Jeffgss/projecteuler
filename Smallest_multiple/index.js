04eab047d7898017621bc2e8eb373623fe1f30c8
Jefferson Gerson dos Santos <jefferson.santos@ENG-2589.maxmilhas.local>
2019-12-16 17:12:59 -0300

    Finalizando o problema 2

---
+++ Smallest_multiple/index.js
@@ -0,0 +1,19 @@
+let value = 1;
+let condi = true;
+const quant = 10;
+
+
+while (condi) {
+    let cont = 0;
+    for (let n1 = 1; n1 <= quant; n1++) {
+        if (value % n1 === 0) {
+            cont++;
+        }
+    }
+    if (cont === quant) {
+        condi = false;
+        console.log(value);
+    }
+
+    value++
+}
