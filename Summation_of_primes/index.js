4e4555f3ad0c03e8ff7b07c90ba687978d1d8d49
Jefferson Gerson dos Santos <jefferson.santos@ENG-2589.maxmilhas.local>
2019-12-20 18:39:04 -0300

    Finalizando o problema 10

--- Summation_of_primes/index.js
+++ Summation_of_primes/index.js
@@ -1,19 +1,21 @@
 const primeNumbers = [];
 let num = 2;
 let result = 0;
+let sumPrime = 0;
+let total = 2000001;
 
-while (primeNumbers.length < 10) {
+while (num < total) {
     let primeFlag = true;
     for (let count = 2; count < num; count++) {
-        if(num % count === 0) {
+        if (num % count === 0) {
             primeFlag = false;
             break;
-            
+
         }
     }
-    if(primeFlag) {
-        primeNumbers.push(num);
+    if (primeFlag) {
+        sumPrime += num;
     }
     num++;
 }
-console.log(primeNumbers);
+console.log(sumPrime);
