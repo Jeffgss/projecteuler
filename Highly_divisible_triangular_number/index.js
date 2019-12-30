c6cf998f4a8034b7fa3dd3b52f6c6a3e1d621eb0
Jefferson Gerson dos Santos <jefferson.santos@ENG-2589.maxmilhas.local>
2019-12-30 11:22:19 -0300

    Finalizando o problema 12

---
+++ Highly_divisible_triangular_number/index.js
@@ -0,0 +1,22 @@
+let objetivo = false;
+let contador = 1;
+const limiteDivisores = 500;
+
+function verificaSeExiste500Divisores(numero) {
+    let divisores = 0;
+    for (let index = numero; index > 1; index--) {
+        if (numero % index === 0) {
+            divisores++;
+        }
+    }
+    return divisores >= limiteDivisores;
+}
+
+while (objetivo === false) {
+    let triangular = contador * (contador + 1) / 2;
+    if (verificaSeExiste500Divisores(triangular)) {
+        objetivo = true;
+        console.log(`Cheguei ao objetivo, o número ${triangular} é o primeiro triagular a ter ${limiteDivisores} divisores`);
+    }
+    contador++;
+}
